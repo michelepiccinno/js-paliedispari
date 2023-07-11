db4c5ffa5f962eaa46666211361d8e807f340cd1
michelepiccinno <michele.piccinno@gmail.com>
2023-07-11 02:26:56 +0200

    add-html

--- palidroma/script.js
+++ palidroma/script.js
@@ -3,12 +3,26 @@ Chiedere all’utente di inserire una parola
 Creare una funzione per capire se la parola inserita è palindroma (una parola che 
   si legge allo stesso modo anche da destra versi sinistra, ad esempio anna, otto)*/
 
-//CHIEDO ALL'UTENTE DI INSERIRE UNA PAROLA
-const word = prompt('Inserisci una parola');
+//RECUPERO GLI ELEMENTI E LI ASSEGNO ALLE VARIABILI
+const inputWordElement = document.querySelector("[name='word-input']");
+const btnInvia = document.querySelector(".invia-dati");
 
-//RICHIAMO LA FUNZIONE checkPalidnrome CHE VERIFICA SE E' PALINDROMA
-const checkedWord = checkPalidnrome(word);
-console.log(checkedWord);
+//CREO L'EVENTLISTENER
+btnInvia.addEventListener("click", function () {
+  const userWord = inputWordElement.value;
+
+  //RICHIAMO LA FUNZIONE checkPalidnrome CHE VERIFICA SE E' PALINDROMA
+  const checkedWord = checkPalidnrome(userWord);
+  console.log(checkedWord);
+
+  //INIETTO OUTPUT MSG NELL'HTML
+  if (checkedWord == true) {
+    document.getElementById("output-msg").innerHTML = (`la parola ${userWord} è palindroma`);
+  } else {
+    document.getElementById("output-msg").innerHTML = (`la parola ${userWord} NON è palindroma`);
+  }
+})
+
 
 //CREO UNA FUNZIONE CHE VERIFICA SE UNA PAROLA E' PALINDROMA
 /**Riceve una stringa, e restituisce true se palindroma, viceversa false
--- pari-e-dispari/script.js
+++ pari-e-dispari/script.js
@@ -5,33 +5,49 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.*/
 
-//CHIEDE ALL'UTENTE DI INSERIRE UN NUMERO
-const inputNum = parseInt(prompt('Inserisci un numero da 1 a 5'), 10);
-const inputEvenOdd = prompt('Scrivi "pari" o "dispari"');
-console.log(`Numero scelto dal giocatore: ${inputNum}`);
-console.log(`il giocatore ha scelto : ${inputEvenOdd}`);
+//RECUPERO GLI ELEMENTI E LI ASSEGNO ALLE VARIABILI
+const inputNumElement = document.querySelector("[name='number-input']");
+const inputEvenOddElement = document.querySelector("#select-evenodd");
+const btnInvia = document.querySelector(".invia-dati");
 
-//INVOCO LA FUNZIONE CHE GENERA UN RANDOM NUMBER DA 1 A 5
-const pcNumber = numRandom1to5();
-console.log(`Numero assegnato al PC: ${pcNumber}`);
+//CREO L'EVENTLISTENER
+btnInvia.addEventListener("click", function () {
+  const userNum = parseInt(inputNumElement.value);
+  const EvenOddSelect = inputEvenOddElement.value;
+  console.log(`Numero scelto dal giocatore: ${userNum}`);
+  console.log(`il giocatore ha scelto : ${EvenOddSelect}`);
 
-//ALLA VARIABILE SUM ASSEGNIAMO LA SOMMA DEI DUE VALORI
-const sum = inputNum + pcNumber;
-console.log(`la somma dei due numeri è: ${sum} `)
+  //VERIFICO SE IL NUMERO INSERITO E' MAGGIORE/MINORE DI 5
+  //VERIFICO SE L'UTENTE HA INSERITO TYPE NUMBER
+  if ((inputNumElement.value < 1) || (inputNumElement.value > 5)) {
+    return alert('inserisci un numero minore o uguale a 5!');
+  } /*else if (typeof inputNumElement.value !== 'number') {           DA VERIFICARE PERCHE' NON VA
+    return alert('non sono ammessi caratteri diversi da numeri!');
+  }*/
 
-//INVOCO LA FUNZIONE checkEvenOdd CHE VERIFICA SE LA SOMMA E' PARI O DISPARI
-const numToCheck = checkEvenOdd(sum);
-console.log(`True = pari, False = dispari: ${numToCheck}`);
+  //INVOCO LA FUNZIONE CHE GENERA UN RANDOM NUMBER DA 1 A 5 PER IL PC
+  const pcNumber = numRandom1to5();
+  console.log(`Numero assegnato al PC: ${pcNumber}`);
+  document.getElementById("pc-num").innerHTML = (`Numero assegnato al PC: ${pcNumber}`);
 
-//VERIFICHIAMO CHI HA VINTO 
-if ((inputEvenOdd == 'pari') && (mod == true)) {
-  console.log('ha vinto il giocatore');
-} else if ((inputEvenOdd == 'dispari') && (mod == false)) {
-  console.log('ha vinto il giocatore');
-} else {
-  console.log('ha vinto il PC');
-}
+  //ALLA VARIABILE SUM ASSEGNIAMO LA SOMMA DEI DUE VALORI
+  const sum = userNum + pcNumber;
+  console.log(`la somma dei due numeri è: ${sum} `)
+  document.getElementById("somma").innerHTML = (`la somma dei due numeri è: ${sum} `);
 
+  //INVOCO LA FUNZIONE checkEvenOdd CHE VERIFICA SE LA SOMMA E' PARI O DISPARI
+  const checkResult = checkEvenOdd(sum);
+
+  //VERIFICHIAMO CHI E' IL VINCITORE
+  if (((EvenOddSelect == 'pari') && (checkResult == true)) ||
+    ((EvenOddSelect == 'dispari') && (checkResult == false))) {
+    console.log('ha vinto il giocatore');
+    document.getElementById("winner").innerHTML = ('ha vinto il giocatore!');
+  } else {
+    console.log('ha vinto il PC');
+    document.getElementById("winner").innerHTML = ('ha vinto il PC!');
+  }
+})
 
 /*FUNZIONI*/
 
@@ -40,8 +56,8 @@ if ((inputEvenOdd == 'pari') && (mod == true)) {
  * @returns
  */
 function numRandom1to5() {
-  const num = Math.floor((Math.random() * 5) + 1)
-  return num;
+  const randomNum = Math.floor((Math.random() * 5) + 1)
+  return randomNum;
 }
 
 //CREO UNA FUNZIONE CHE VERIFICA SE UN NUMERO è PARI O DISPARI
@@ -57,3 +73,4 @@ function checkEvenOdd(num) {
   }
   return mod;
 }
+
